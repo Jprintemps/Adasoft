d139bb1c61d732c802681f9a57db0770186fec0d
Printemps <julexcess@gmail.com>
2026-05-18 13:57:39 +0300

    new update

--- api/marchand.php
+++
@@ -1,6 +0,0 @@
-<?php
-$marchand = array(
-    "apikey" => "1047138914666d714c81fe79.38656040", // Enrer votre apikey
-    "site_id" => "105906657", //Entrer votre site_ID
-    "secret_key" => "61602386968d449411b0234.29662521" //Entrer votre clé secret
-);
